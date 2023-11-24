190d9f14b085cc4a2b421b47ce68f11afdff6bb2
rasel <rasel.cse19.ruet@gmail.com>
2023-11-24 15:50:59 +0600

    topbar + floating Action button added

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -3,15 +3,29 @@ package com.example.todoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.todoapp.ui.theme.TodoAppTheme
+import com.example.todoapp.ui.theme.blackFont
+import com.example.todoapp.ui.theme.topBarColor
+import com.example.todoapp.ui.theme.whiteContainer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,13 +38,46 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Scaffold(
-
-
+                        topBar = { TopBar()},
+                        floatingActionButton = { FloatinActionBtn()},
+                        floatingActionButtonPosition = FabPosition.End
                     ) {paddingValues ->
-
+                        Column(modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues)) {
+                            Text(text = "HomeScreen")
+                        }
                     }
                 }
             }
         }
     }
+}
+
+@Composable
+fun FloatinActionBtn() {
+    FloatingActionButton(
+        onClick = {  },
+        containerColor = whiteContainer,
+        modifier = Modifier.padding(10.dp)
+    ) {
+        Text(
+            text = "Add Task",
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 20.sp,
+            color = blackFont,
+            modifier = Modifier.padding(10.dp)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = { Text(text = "Todo") },
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = topBarColor
+        )
+    )
 }
--- app/src/main/java/com/example/todoapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/todoapp/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val blackFont = Color(0xFF504C4C)
+val whiteContainer = Color(0xFFFFFFFF)
+val whiteFont = Color(0xFFEEE4E4)
+val topBarColor = Color(0xFF008FFF)
